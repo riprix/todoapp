c02744dad61d8af7b8e0e61a7a2cf9746cfae228
„riprix“ <thomas.saer@googlemail.com>
2014-02-05 16:59:19 +0100

    Task Scaffold und Seed Daten

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# ruby encoding: utf-8
+Task.create(name: "Todo-Applikation", deadline: Date.today + 7.days, duration: 2, done: false)
+Task.create(name: "Idee für eigene Web-Applikation", deadline: Date.today + 10.days, duration: 2, done: false)
+Task.create(name: "Rails for Zombies", deadline: Date.today - 2.days, duration: 3, done: false)
+Task.create(name: "Übung 6: Rails Account", deadline: Date.today - 4.days, duration: 3, done: false)
+Task.create(name: "Übung 1: FizzBuzz", deadline: Date.today - 26.days, duration: 4, done: true)
+Task.create(name: "Übung 2: Ruby Konto", deadline: Date.today - 20.days, duration: 5, done: true)
